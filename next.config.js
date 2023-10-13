c63ac350e4b6d086657397bcbf6c139832552b05
viralgupta <guptaviral0@gmail.com>
2023-10-13 12:49:33 +0530

    url

--- next.config.js
+++ next.config.js
@@ -3,23 +3,23 @@ module.exports = () => {
     return [
       {
         source: "/api/user/login",
-        destination: "https://2e8f-14-96-13-220.ngrok-free.app/api/user/login",
+        destination: "https://74e4-14-96-13-220.ngrok-free.app/api/user/login",
       },
       {
         source: "/api/user/signup",
-        destination: "https://2e8f-14-96-13-220.ngrok-free.app/api/user/signup",
+        destination: "https://74e4-14-96-13-220.ngrok-free.app/api/user/signup",
       },
       {
         source: "/api/user/bookevent",
-        destination: "https://2e8f-14-96-13-220.ngrok-free.app/api/user/bookevent",
+        destination: "https://74e4-14-96-13-220.ngrok-free.app/api/user/bookevent",
       },
       {
         source: "/api/user/myevents",
-        destination: "https://2e8f-14-96-13-220.ngrok-free.app/api/user/myevents",
+        destination: "https://74e4-14-96-13-220.ngrok-free.app/api/user/myevents",
       },
       {
         source: "/api/event/getevents",
-        destination: "https://2e8f-14-96-13-220.ngrok-free.app/api/event/getevents",
+        destination: "https://74e4-14-96-13-220.ngrok-free.app/api/event/getevents",
       }
     ];
   };
